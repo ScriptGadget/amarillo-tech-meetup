82edf98853bf5e6212face150cd08ac6ca519321
Bill Glover <bill.the.writer@gmail.com>
2018-03-20 19:30:05 -0500

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
     statusCode: 200,
-    body: "Hello, " + event.queryStringParameters['email']
+    body: "Invitation sent, " + event.queryStringParameters['email']
     });
 }
